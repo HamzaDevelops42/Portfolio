490638617162f5c0598ce89f71056aa968cbfc70
Hamza <smarttalk4242@gmail.com>
2025-10-25 17:48:23 +0500

    add light rays background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import HorizontalScroll from "@/components/HorizontalScroll";
+import LightRays from "@/components/LightRays";
 import LiquidEther from "@/components/LiquidEther";
 import { Navbar } from "@/components/Navbar";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
@@ -11,23 +12,19 @@ export default function Page() {
 
       {/* <Navbar /> */}
       <ThemeSwitcher />
-      <div style={{ width: '100%', height: "100vh", position: 'fixed' }}>
-        <LiquidEther
-          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
+
+      <div style={{ width: '100%', height: '100vh', position: 'fixed' }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#00ffff"
+          raysSpeed={2.3}
+          lightSpread={0.28}
+          rayLength={3}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="custom-rays"
         />
       </div>
       <HorizontalScroll />
