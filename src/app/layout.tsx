250c850f861809a54acb5c7ad1307ca15a0f2d09
Hamza <smarttalk4242@gmail.com>
2025-11-11 18:19:07 +0500

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import Providers from "./providers";
 import { Toaster } from "@/components/ui/sonner"
 import { GoogleAnalytics } from '@next/third-parties/google'
